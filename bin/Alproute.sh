d8df1a862e845514ee58abd38a96394166aad2e1
Alex <plcgi1@gmail.com>
2011-11-03 19:19:25 +0300

    Update bin/Alproute.sh

--- bin/Alproute.sh
+++ bin/Alproute.sh
@@ -1,1 +1,1 @@
-plackup -I/home/harper/www/woa-toolkit/core/lib -I/home/harper/www/woa-toolkit/apps/Alproute/lib -I/home/harper/www/woa-toolkit/plack/lib -p 3030 -a /home/harper/www/woa-toolkit/apps/Alproute/psgi/Alproute.psgi 
+plackup -I/usr/local/woa-toolkit/core/lib -I/usr/local/woa-toolkit/apps/Alproute/lib -I//usr/local/woa-toolkit/plack/lib -p 3030 -a /usr/local/woa-toolkit/apps/Alproute/psgi/Alproute.psgi 
